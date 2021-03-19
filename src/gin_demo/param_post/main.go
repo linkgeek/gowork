b04f6953818fffeda9e6cfbd1ee35c5fc9adf644
hezhan <941192051@qq.com>
2021-03-19 20:55:14 +0800

    新学习

--- src/gin_demo/param_post/main.go
+++ src/gin_demo/param_post/main.go
@@ -5,10 +5,10 @@ import "github.com/gin-gonic/gin"
 func main()  {
 	r:=gin.Default()
 	r.POST("/info", func(c *gin.Context) {
-		uname := c.PostForm("uname")
+		uname := c.PostForm("name")
 		age := c.DefaultPostForm("age", "18")
 		c.String(200, "%s, %s", uname, age)
 	})
 	r.Run()
 }
-// curl -X POST "http://127.0.0.1:8080/info" -d "{uname: "giant", age: 18}"
+// curl -X POST "http://127.0.0.1:8080/info" -d "name=giant&age=28"
