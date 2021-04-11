eb049072f07c65518098d82e6748508bead3b2c9
linkgeek <941192051@qq.com>
2021-04-12 00:03:23 +0800

    bankproject

--- src/chapter/challenge/bankapi/main.go
+++ src/chapter/challenge/bankapi/main.go
@@ -2,10 +2,107 @@ package main
 
 import (
     "fmt"
-
+    "net/http"
     "github.com/msft/bank"
+    "strconv"
+    "log"
 )
 
+var accounts = map[float64]*bank.Account{}
+
+// 公开对账单方法 http://localhost:8000/statement?number=1001
+func statement(w http.ResponseWriter, req *http.Request) {
+    numberqs := req.URL.Query().Get("number")
+
+    if numberqs == "" {
+        fmt.Fprintf(w, "Account number is missing!")
+        return
+    }
+
+    if number, err := strconv.ParseFloat(numberqs, 64); err != nil {
+        fmt.Fprintf(w, "Invalid account number!")
+    } else {
+        account, ok := accounts[number]
+        if !ok {
+            fmt.Fprintf(w, "Account with number %v can't be found!", number)
+        } else {
+            fmt.Fprintf(w, account.Statement())
+        }
+    }
+}
+
+// 公开存款方法 
+// http://localhost:8000/deposit?number=1001&amount=10
+// curl http://localhost:8000/deposit?number=1001&amount=100
+func deposit(w http.ResponseWriter, req *http.Request) {
+    numberqs := req.URL.Query().Get("number")
+    amountqs := req.URL.Query().Get("amount")
+
+    if numberqs == "" {
+        fmt.Fprintf(w, "Account number is missing!")
+        return
+    }
+
+    if number, err := strconv.ParseFloat(numberqs, 64); err != nil {
+        fmt.Fprintf(w, "Invalid account number!")
+    } else if amount, err := strconv.ParseFloat(amountqs, 64); err != nil {
+        fmt.Fprintf(w, "Invalid amount number!")
+    } else {
+        account, ok := accounts[number]
+        if !ok {
+            fmt.Fprintf(w, "Account with number %v can't be found!", number)
+        } else {
+            err := account.Deposit(amount)
+            if err != nil {
+                fmt.Fprintf(w, "%v", err)
+            } else {
+                fmt.Fprintf(w, account.Statement())
+            }
+        }
+    }
+}
+
+// 公开取款方法 http://localhost:8000/withdraw?number=1001&amount=100
+func withdraw(w http.ResponseWriter, req *http.Request) {
+    numberqs := req.URL.Query().Get("number")
+    amountqs := req.URL.Query().Get("amount")
+
+    if numberqs == "" {
+        fmt.Fprintf(w, "Account number is missing!")
+        return
+    }
+
+    if number, err := strconv.ParseFloat(numberqs, 64); err != nil {
+        fmt.Fprintf(w, "Invalid account number!")
+    } else if amount, err := strconv.ParseFloat(amountqs, 64); err != nil {
+        fmt.Fprintf(w, "Invalid amount number!")
+    } else {
+        account, ok := accounts[number]
+        if !ok {
+            fmt.Fprintf(w, "Account with number %v can't be found!", number)
+        } else {
+            err := account.WithDraw(amount)
+            if err != nil {
+                fmt.Fprintf(w, "%v", err)
+            } else {
+                fmt.Fprintf(w, account.Statement())
+            }
+        }
+    }
+}
+
 func main() {
-    fmt.Println(bank.Hello())
+    accounts[1001] = &bank.Account{
+        Customer: bank.Customer{
+            Name: "John",
+            Address: "ShenZhen",
+            Phone: "183xxxx",
+        },
+        Number: 1001,
+    }
+
+    http.HandleFunc("/statement", statement)
+    http.HandleFunc("/deposit", deposit)
+    http.HandleFunc("/withdraw", withdraw)
+    log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
---
+++ src/chapter/challenge/bankcore/bank_test.go
@@ -0,0 +1,97 @@
+package bank
+
+import "testing"
+
+func TestAccount(t *testing.T) {
+	account := Account{
+        Customer: Customer{
+            Name:    "John",
+            Address: "Los Angeles, California",
+            Phone:   "(213) 555 0147",
+        },
+        Number:  1001,
+        Balance: 0,
+    }
+
+    if account.Name == "" {
+        t.Error("can't create an Account object")
+    }
+}
+
+// 测试存款方法
+func TestDeposit(t *testing.T) {
+	account := Account{
+        Customer: Customer{
+            Name:    "John",
+            Address: "Los Angeles, California",
+            Phone:   "(213) 555 0147",
+        },
+        Number:  1001,
+        Balance: 0,
+	}
+	
+	account.Deposit(10)
+
+    if account.Balance != 10 {
+        t.Error("balance is not being updated after a deposit")
+    }
+}
+
+func TestDepositInvalid(t *testing.T) {
+	account := Account{
+        Customer: Customer{
+            Name:    "John",
+            Address: "Los Angeles, California",
+            Phone:   "(213) 555 0147",
+        },
+        Number:  1001,
+        Balance: 0,
+	}
+	
+	
+
+    if err := account.Deposit(-10); err == nil {
+        t.Error("only positive numbers should be allowed to deposit")
+    }
+}
+
+// 测试取款方法
+func TestWithDraw(t *testing.T)  {
+    account := Account{
+        Customer: Customer{
+            Name:    "John",
+            Address: "Los Angeles, California",
+            Phone:   "(213) 555 0147",
+        },
+        Number:  1001,
+        Balance: 0,
+	}
+	
+	account.Deposit(10)
+	account.WithDraw(10)
+
+    if account.Balance != 0 {
+        t.Error("balance is not being updated after withdraw")
+    }
+}
+
+// 对账单方法
+func TestStatement(t *testing.T)  {
+    account := Account{
+        Customer: Customer{
+            Name:    "John",
+            Address: "Los Angeles, California",
+            Phone:   "(213) 555 0147",
+        },
+        Number:  1001,
+        Balance: 0,
+	}
+	
+	account.Deposit(100)
+	statement := account.Statement()
+    if statement != "1001 - John - 100" {
+        t.Error("statement doesn't have the proper format")
+    }
+}
+
+// go test -v
